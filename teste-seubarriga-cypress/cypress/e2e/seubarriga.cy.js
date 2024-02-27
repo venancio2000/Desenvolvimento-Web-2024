4e03671b4a5614845c30b305532c02e67d73afd0
venancio2000 <jbrunogvenancio@gmail.com>
2024-02-26 22:29:11 -0300

    subida do cypress

---
+++ teste-seubarriga-cypress/cypress/e2e/seubarriga.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+
+describe('Shoud test at a functional level', () => {
+  before(() => {
+    cy.visit('https://barrigareact.wcaquino.me')
+    cy.get('[data-test="email"]').type('jbrunogvenancio@gmail.com')
+      cy.get('[data-test="passwd"]').type('Br#91801207', )
+      cy.get('.btn').click()
+      cy.get('.toast-message').should('contain', 'Bem vindo')
+  })  
+  
+  it('should create an account ', () => {
+      
+    cy.get('[data-test="menu-settings"]').click()
+    cy.get('[href="/contas"]').click()
+    cy.get('[data-test="nome"]').type('conta de teste')
+    cy.get('.btn').click()
+    cy.get('.toast-success > .toast-message').should('contain', 'Conta inserida com sucesso')
+      
+    })
+
+  })
